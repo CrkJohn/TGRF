4b40b6684c509de89b1f49571399ee7011869001
John David Ibañez <JhonhRge@gmail.com>
2018-09-18 17:12:46 -0500

    Create B - Subway.cpp

---
+++ Contest5/B - Subway.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define FOR(i,b) for (int i=0; i<b; i++)
+#define loop(i,a,b) for (int i=a; i<b; i++)
+#define len(a) ((int)a.size())
+#define fi first
+#define se second
+#define pb push_back
+#define lectura() freopen("in.txt","r",stdin); freopen("out.txt","w",stdout)
+
+
+using namespace std;
+
+typedef pair<int,int> ii;
+
+
+int main(){
+    lectura();
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	stringstream ss;
+	string str;
+	int ntc,srcx,srcy,tgtx,tgty;
+	cin >>  ntc;
+	getline(cin,str);
+	getline(cin,str);
+	while(ntc--){
+        getline(cin,str);
+        ss.clear();
+        ss << str;
+        ss >> srcx >> srcy >> tgtx >> tgty;
+        while(1){
+            getline(cin,str);
+            if(str=="")break;
+            ss.clear();
+            ss << str;
+
+        }
+	}
+	return 0;
+}
